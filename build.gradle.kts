101726dc50eef3a36e4fa05bc903c4346958c7b9
Dominic Fischer <dominicfischer7@gmail.com>
2019-09-01 15:24:29 +0100

    Add temp hack.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinTest
+
 plugins {
     kotlin("multiplatform") version "1.3.41" apply false
     id("kotlinx-serialization") version "1.3.41" apply false
@@ -11,4 +13,11 @@ subprojects {
 
     group = "io.github.matrixkt"
     version = "0.0.1"
+
+    //TODO: Remove after Kotlin 1.3.50 fixes https://youtrack.jetbrains.net/issue/KT-33246
+    afterEvaluate {
+        tasks.withType<KotlinTest> {
+            binaryResultsDirectory.set(binResultsDir)
+        }
+    }
 }
