e99d6aff6baa7fafc0ebaec8834e2f8b77af1621
Dominic Fischer <dominicfischer7@gmail.com>
2020-04-07 18:22:54 +0100

    Kotlin 1.3.71

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("multiplatform") version "1.3.70" apply false
-    kotlin("plugin.serialization") version "1.3.70" apply false
+    kotlin("multiplatform") version "1.3.71" apply false
+    kotlin("plugin.serialization") version "1.3.71" apply false
 }
 
 val ktorVersion: String by extra("1.3.2")
 val serialVersion: String by extra("0.20.0")
-val coroutineVersion: String by extra("1.3.4")
+val coroutineVersion: String by extra("1.3.5")
 val jnaVersion: String by extra("5.5.0")
 
 subprojects {
